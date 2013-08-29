61754ac11fabd1f7cafe7d3785ab88234cc266ab
boomsb <bradley.booms@gmail.com>
2013-08-29 10:04:30 -0500

    Update pageable_mixin.js

--- pageable_mixin.js
+++ pageable_mixin.js
@@ -1,126 +1,122 @@
-var get = Ember.get,
-    set = Ember.set;
+var get = Ember.get, set = Ember.set;
 
 /**
-  `Ember.PageableMixin` provides a standard interface to narrow the view of
-  data to a given page, as a function of `pageNumber`, `pageSize`, and content.
-
-  controller = Ember.ArrayController.extend(Ember.PageableMixin, {
-    content: [ 1, 2, 3 ],
-  });
-
-  controller.set('pageSize', 2);
-
-  ```
-
-  @class PageableMixin
-  @namespace Ember
-  @extends Ember.Mixin
-  @uses Ember.MutableEnumerable
-  */
+ * `Ember.PageableMixin` provides a standard interface to narrow the view of
+ * data to a given page, as a function of `pageNumber`, `pageSize`, and content.
+ * 
+ * controller = Ember.ArrayController.extend(Ember.PageableMixin, { content: [
+ * 1, 2, 3 ], });
+ * 
+ * controller.set('pageSize', 2);
+ * 
+ * ```
+ * 
+ * @class PageableMixin
+ * @namespace Ember
+ * @extends Ember.Mixin
+ * @uses Ember.MutableEnumerable
+ */
 
 Ember.PageableMixin = Ember.Mixin.create(Ember.MutableEnumerable, {
-    pageNumber: 0,
-    pageSize: 10,
-    _viewChanged: false, // current hackish approach to determine when arranged
-                         // content must be updated. this should only happen if
-                         // the range of items within the current view would
-                         // change
+  pageNumber : 0,
+  pageSize : 10,
+  _viewChanged : false, // current hackish approach to determine when arranged
+  // content must be updated. this should only happen if
+  // the range of items within the current view would
+  // change
 
-    /**
-     * Gets number of pages.
-     */
-    pageCount: Ember.computed('arrangedContent.length', 'pageSize', function () {
+  /**
+   * Gets number of pages.
+   */
+  pageCount : Ember.computed('arrangedContent.length', 'pageSize', function() {
 
-        return Math.ceil(get(this, 'arrangedContent.length') /
-            get(this, 'pageSize'));
+    return Math.ceil(get(this, 'arrangedContent.length') / get(this, 'pageSize'));
 
-    }).cacheable(),
+  }).cacheable(),
 
-    pages: function() {
+  pages : function() {
 
-      var pageCount = this.get('pageCount'),
-      pages = [],
-      page;
+    var pageCount = this.get('pageCount'), pages = [], page;
 
-      for (i = 0; i < pageCount; i++) {
-        page = i + 1;
-        pages.push(page);
-      }
+    for (i = 0; i < pageCount; i++) {
+      page = i + 1;
+      pages.push(page);
+    }
 
-      return pages;
+    return pages;
 
-    }.property('pageCount'),
-    
-    _resetPageToZero: Ember.observer(function () {
-        // what does it mean for page size to change and for us to be on
-        // page X? there is no way to figure this out, so we default to
-        // setting page back to 0
+  }.property('pageCount'),
 
-        set(this, 'pageNumber', 0);
-    }, 'pageSize', 'arrangedContent'),
+  showPager : Ember.computed.gt('pageCount', 1),
 
-    contentArrayDidChange: function(array, idx, removedCount, addedCount) {
-        var pageNumber = get(this, 'pageNumber');
-        var pageSize = get(this, 'pageSize');
+  _resetPageToZero : Ember.observer(function() {
+    // what does it mean for page size to change and for us to be on
+    // page X? there is no way to figure this out, so we default to
+    // setting page back to 0
 
-        var start = pageNumber * pageSize;
-        var end = start + pageSize;
+    set(this, 'pageNumber', 0);
+  }, 'pageSize', 'arrangedContent'),
 
-        if (idx <= end && idx + removedCount + addedCount >= start) {
-            this.toggleProperty('_viewChanged');
-        }
+  contentArrayDidChange : function(array, idx, removedCount, addedCount) {
+    var pageNumber = get(this, 'pageNumber');
+    var pageSize = get(this, 'pageSize');
 
-        return this._super(array, idx, removedCount, addedCount);
-    },
+    var start = pageNumber * pageSize;
+    var end = start + pageSize;
 
-    pagedContent: Ember.computed('_viewChanged', 'arrangedContent', 'pageNumber', 'pageSize', function () {
-        var content = get(this, 'arrangedContent');
-        if (! content) {
-            return Ember.A([]);
-        }
+    if (idx <= end && idx + removedCount + addedCount >= start) {
+      this.toggleProperty('_viewChanged');
+    }
 
-        var pageSize = get(this, 'pageSize');
-        var start = get(this, 'pageNumber') * pageSize;
-        var end = start + pageSize;
-        return content.slice(start, end);
-    }).cacheable(),
+    return this._super(array, idx, removedCount, addedCount);
+  },
 
-    /**
-     * Decrements pageNumber. If pageNumber is already 0, does nothing.
-     */
-    decrementPage: function () {
-        this.set('pageNumber', Math.max(get(this, 'pageNumber') - 1, 0));
-    },
+  pagedContent : Ember.computed('_viewChanged', 'arrangedContent', 'pageNumber', 'pageSize', function() {
+    var content = get(this, 'arrangedContent');
+    if (!content) {
+      return Ember.A([]);
+    }
 
-    /**
-     * Set pageNumber to next possible. Does not increment if on highest pageNumber.
-     */
-    incrementPage: function () {
-        set(this, 'pageNumber', Math.min(
-                    get(this, 'pageNumber') + 1,
-                    get(this, 'pageCount') - 1));
-    },
+    var pageSize = get(this, 'pageSize');
+    var start = get(this, 'pageNumber') * pageSize;
+    var end = start + pageSize;
+    return content.slice(start, end);
+  }).cacheable(),
 
-    /**
-     * Set pageNumber to visible page value (pageNumber - 1).
-     */
-    setPage: function (pageNumber) {
-      set(this, 'pageNumber', parseInt(pageNumber, 10) - 1);
-    },
+  /**
+   * Decrements pageNumber. If pageNumber is already 0, does nothing.
+   */
+  decrementPage : function() {
+    this.set('pageNumber', Math.max(get(this, 'pageNumber') - 1, 0));
+  },
 
-    /**
-     * Returns if the current pageNumber is the first.
-     */
-    onFirstPage: Ember.computed('pageNumber', function () {
-        return get(this, 'pageNumber') === 0;
-    }).cacheable(),
+  /**
+   * Set pageNumber to next possible. Does not increment if on highest
+   * pageNumber.
+   */
+  incrementPage : function() {
+    set(this, 'pageNumber', Math.min(get(this, 'pageNumber') + 1, get(this, 'pageCount') - 1));
+  },
 
-    /**
-     * Returns if the current pageNumber is the last.
-     */
-    onLastPage: Ember.computed('pageNumber', 'pageCount', function () {
-        return get(this, 'pageNumber') === get(this, 'pageCount') - 1;
+  /**
+   * Set pageNumber to visible page value (pageNumber - 1).
+   */
+  setPage : function(pageNumber) {
+    set(this, 'pageNumber', parseInt(pageNumber, 10) - 1);
+  },
+
+  /**
+   * Returns if the current pageNumber is the first.
+   */
+  onFirstPage : Ember.computed('pageNumber', function() {
+    return get(this, 'pageNumber') === 0;
+  }).cacheable(),
+
+  /**
+   * Returns if the current pageNumber is the last.
+   */
+  onLastPage : Ember.computed('pageNumber', 'pageCount', function() {
+    return get(this, 'pageNumber') === get(this, 'pageCount') - 1;
   }).cacheable()
 });
 
